1f31c683a3c899c9154d713275f5c8ef7961faec
Thing <133474899+Sjdjdiejdrirhdkjej@users.noreply.github.com>
2026-02-04 02:44:46 +0000

    chore: remove shell scripts

--- start-tidewave.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
-export PUPPETEER_EXECUTABLE_PATH=$(which chromium)
-export CHROMIUM_FLAGS="--no-sandbox --disable-setuid-sandbox"
-
-# Download and run Tidewave CLI
-curl -sL https://github.com/tidewave-ai/tidewave_app/releases/latest/download/tidewave-cli-x86_64-unknown-linux-gnu -o /tmp/tidewave-cli
-chmod +x /tmp/tidewave-cli
-/tmp/tidewave-cli --allow-remote-access "$@"
