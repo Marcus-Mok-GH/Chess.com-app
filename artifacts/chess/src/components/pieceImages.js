76188305e0feabb920b4c43fa27ccd707f076f4f
Marcus Mok <133474899+Marcus-Mok-GH@users.noreply.github.com>
2026-09-11 12:22:50 +0800

    Add shared chess piece image paths

---
+++ artifacts/chess/src/components/pieceImages.js
@@ -0,0 +1,14 @@
+export const PIECE_IMAGE_PATHS = {
+  wK: '/custom-pieces/wK.svg',
+  wQ: '/custom-pieces/wQ.svg',
+  wR: '/custom-pieces/wR.svg',
+  wB: '/custom-pieces/wB.svg',
+  wN: '/custom-pieces/wN.svg',
+  wP: '/custom-pieces/wP.svg',
+  bK: '/custom-pieces/bK.svg',
+  bQ: '/custom-pieces/bQ.svg',
+  bR: '/custom-pieces/bR.svg',
+  bB: '/custom-pieces/bB.svg',
+  bN: '/custom-pieces/bN.svg',
+  bP: '/custom-pieces/bP.svg',
+};
